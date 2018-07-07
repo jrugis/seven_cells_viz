dcba17c8be4e77d336991ae1ec2bc572c031a71b
jrugis <j.rugis@auckland.ac.nz>
2018-07-07 18:04:43 +1200

    all data!

--- mat2vtk.py
+++ mat2vtk.py
@@ -8,6 +8,7 @@ import os
 import scipy.io as sc
 import struct
 import subprocess
+import time
 from evtk.hl import pointsToVTK
 
 #import cs
@@ -26,15 +27,26 @@ def read_bin(fname):
   f1.close # close the binary file 
   return verts
 
+###########################################################################
+def write_points(fname, verts, d):
+  nverts = verts.shape[0]
+  xyz = np.empty([3, nverts]) # needs re-ordering
+  for i in range(nverts):
+    for j in range(3):
+      xyz[j,i] = verts[i,j]  
+  pointsToVTK(fname, \
+    xyz[0,:], xyz[1,:], xyz[2,:], \
+    data=d)  # write out vtu file
+  return
+
 ##################################################################
 # main program
 ##################################################################
 
-cell_num = 1
-mesh_name = '4sim_out_N4_p3-p2-p4-' + str(cell_num) + 'tet.bin'
 dist_name = 'Ca_1_7.mat'
 dist_key = 'c_all'
 
+start_time = time.time()
 print
 print 'reading data file: ' + dist_name 
 dist = sc.loadmat(dist_name)
@@ -42,34 +54,37 @@ dist = sc.loadmat(dist_name)
 node_data = dist[dist_key]
 print node_data.shape
 
-print
-print 'cell number: ', cell_num
-print 'reading binary mesh file: ' + mesh_name
-verts = read_bin(mesh_name) 
-print verts.shape
+for cell_num in range(1,8):
+  print
+  print 'cell number: ', cell_num
+  mesh_name = '4sim_out_N4_p3-p2-p4-' + str(cell_num) + 'tet.bin'
+  print 'reading binary mesh file: ' + mesh_name
+  verts = read_bin(mesh_name)
+  print verts.shape
 
-c_data = np.transpose(node_data[0, cell_num-1])
-print c_data.shape
-print 'max =', '{:0.3f}'.format(c_data.max())
-print 'min =', '{:0.3f}'.format(c_data.min())
-p1 = plt.plot(c_data[:,0:10])
-plt.show(p1)
+  c_data = np.transpose(node_data[0, cell_num-1])
+  print c_data.shape
+  print 'max =', '{:0.3f}'.format(c_data.max())
+  print 'min =', '{:0.3f}'.format(c_data.min())
+#  plt.plot(c_data[:,0:10])
+#  plt.show()
 
-## write vtk time series files
-print 'creating vtk time series files...'
-if os.path.isdir(str(cell_num)):
-  os.system("rm -rf " + str(cell_num))
-  os.mkdir(cell_num)
-#i_start = 0
-#i_finish = node_data.shape[0]
-#node_count = node_data.shape[1]
-#for i in xrange(i_start, i_finish, 1):
-#  d = {}
-#  d["c"] = node_data[i, :]
-#  d["dnl"] = dnl
-#  d["ab"] = ab_data
-#  fname = cell_num + '/' + cell_num + '_' + str(i).zfill(4)
-#  #print xyz[0,:].shape
-# #print dist[:, i].shape   
-#  pointsToVTK(fname, xyz[0,:], xyz[1,:], xyz[2,:], data = d)
+  ## write vtk time series files
+  print 'creating vtk time series files'
+  if os.path.isdir(str(cell_num)):
+    os.system("rm -rf " + str(cell_num))
+  os.mkdir(str(cell_num))
+  i_start = 0
+  i_finish = c_data.shape[0]
+  #i_start = 500
+  #i_finish = 800
+
+  c_count = c_data.shape[1]
+  for i in range(i_start, i_finish, 1):
+    d = {}
+    d["c"] = c_data[i,:]
+    fname = str(cell_num) + '/' + str(cell_num) + '_' + str(i).zfill(4)
+    write_points(fname, verts, d)
+
+print 'elapsed time: ', time.time - start_time, ' s'
 
